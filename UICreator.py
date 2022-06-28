af512afdbe063c1f51314d94c57ecee1052402bb
Jacob Duffy <jod0007@auburn.edu>
2022-06-28 16:05:56 -0500

    Creates an UI for user data

---
+++ UICreator.py
@@ -0,0 +1,6 @@
+#Program to create a UI to read in vectorial model data for comet analysis.
+#Uses PyQt5 as the interface to create the UI.
+#This work is based on a pvvectorial repository created by sjoset.
+#
+#Author: Jacob Duffy
+#Version: 6/28/2022
