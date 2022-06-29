98394f0dfdaab9dfae12155bd17275761c5dc5d4
Jacob Duffy <jod0007@auburn.edu>
2022-06-29 13:12:39 -0500

    Program that contains global variables

---
+++ UIVariables.py
@@ -0,0 +1,55 @@
+#Program that contains global variables used in the UI.
+#
+#Author: Jacob Duffy
+#Version: 6/29/2022
+
+DownloadFilePath = None
+ManInputs = False
+FileInputs = False
+
+#Production variables
+BaseQ = None
+TimeVariationType = None
+Amplitude = None
+ParamDelta = None
+Period = None
+
+#Parent variables
+ParentName = None
+VOutflow = None
+TauD = None
+TauTParent = None
+Sigma = None
+TtoDRatio = None
+
+#Fragment variables
+FragmentName = None
+VPhoto = None
+TauTFragment = None
+
+#Comet variables
+CometName = None
+RH = None
+CometDelta = None
+TransformMethod = None
+ApplyTransforMethod = False
+
+#Grid variables
+AngularPoints = None
+RadialPoints = None
+RadialSubsteps = None
+
+#Etc variables
+PrintBinnedTimes = False
+PrintColumnDensity = False
+PrintProgress = False
+PrintRadialDensity = False
+PyvComaPickle = False
+PyvDateOfRun = None
+Show3dColumnDensityCentered = False
+Show3dColumnDensityOffCenter = False
+ShowAgreementCheck = False
+ShowApertureChecks = False
+ShowColumnDensityPlots = False
+ShowFragmentSputter = False
+ShowRadialPlots = False
