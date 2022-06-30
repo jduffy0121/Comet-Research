96ea562a361bf6060463af328943867904cd5f7a
Jacob Duffy <jod0007@auburn.edu>
2022-06-29 20:37:17 -0500

    Creates a yaml file based on user data

--- FileCreator.py
+++ FileCreator.py
@@ -1,39 +1,50 @@
-#Program to create a .yaml file from the results from UIReader.py.
+#Program to create a .yaml file from the results from UIVariables.py.
 #Uses a nested dict data type to write the file.
 #
 #Author: Jacob Duffy
 #Version: 6/29/2022
 
-import yaml
 import UIVariables
+import yaml
+import os
 from datetime import datetime
 
-def createDictory():
+#Method to create a dictionary based on the variables in UIVariables.py
+#Returns the completed dictionary
+def createDictionary():
+
+    #Creates the param sub dictionary used in the production dictory
     params = {'amplitude' : UIVariables.Amplitude, 
     'delta' : UIVariables.ParamDelta,
     'period' : UIVariables.Period}
 
+    #Creates the production dictionary
     production = {'base_q' : UIVariables.BaseQ,
     'time_variation_type' : UIVariables.TimeVariationType, 'params' : params}
 
+    #Creates the parent dictionary
     parent = {'name' : UIVariables.ParentName, 
     'v_outflow' : UIVariables.VOutflow, 
     'tau_d' : UIVariables.TauD, 'tau_T' : UIVariables.TauTParent, 
     'sigma' : UIVariables.Sigma, 'T_to_d_ratio' : UIVariables.TtoDRatio}
 
+    #Creates the fragment dictionary
     fragment = {'name' : UIVariables.FragmentName, 
     'v_photo' : UIVariables.VPhoto, 
     'tau_T' : UIVariables.TauTParent}
 
+    #Creates the comet dictionary
     comet = {'name' : UIVariables.CometName, 'rh' : UIVariables.RH, 
     'delta' : UIVariables.CometDelta, 
     'transform_method' : UIVariables.TransformMethod, 
     'transform_applied' : UIVariables.ApplyTransforMethod}
 
+    #Creates the grid dictionary
     grid = {'radial_points' : UIVariables.RadialPoints, 
     'angular_points' : UIVariables.AngularPoints, 
     'radial_substeps' : UIVariables.RadialSubsteps}
 
+    #Creates the etc dictionary
     etc = {'print_binned_times' : UIVariables.PrintBinnedTimes, 
     'print_column_density' : UIVariables.PrintColumnDensity, 
     'print_progress' : UIVariables.PrintProgress, 
@@ -48,11 +59,17 @@ def createDictory():
     'show_fragment_sputter' : UIVariables.ShowFragmentSputter,
     'show_radial_plots' : UIVariables.ShowRadialPlots}
 
+    #Creates the final dictionary and returns it
     VectorialModelConfig = {'production' : production, 'parent' : parent, 'comet' : comet,
     'fragment' : fragment, 'grid' : grid, 'etc' : etc}
 
     return VectorialModelConfig
 
+#Creates a new .yaml file called data.yaml based on the return val of createDictionary()
 def newFile():
-    with open(r'test.yaml', 'w') as file:
-        documents = yaml.dump(createDictory(), file)
+    with open(r'data.yaml', 'w') as file:
+        documents = yaml.dump(createDictionary(), file)
+
+#Deletes a file named fileName
+def removeFile(fileName):
+    os.remove(fileName)
