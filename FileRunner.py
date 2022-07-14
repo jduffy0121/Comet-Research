0c5b62a1e59e3b5ee36baee9486f9f74c64f674e
Jacob Duffy <jod0007@auburn.edu>
2022-07-14 18:40:58 -0500

    Addedall 5 plots and get methods for extra results

--- FileRunner.py
+++ FileRunner.py
@@ -2,63 +2,49 @@
 #to get results for the vectorial model.
 #
 #Author: Jacob Duffy
-#Version: 7/8/2022
+#Version: 7/14/2022
 
 import UIVariables
 import FileCreator
 import yaml
+import io
 from datetime import datetime
 import astropy.units as u
 from astropy.visualization import quantity_support
+from contextlib import redirect_stdout
 import pyvectorial as pyv
-
-#Method def for converting between km and astro units
-def unitConvert(dataIn):
-    return
 
 #Method def for getting the output results for the vectorial model
 def singleFileRun(fileName):
     quantity_support()
-    #vmc = pyv._vm_config_from_yaml(fileName)
-    #vmc.etc['print_progress'] = True
-    #coma = pyv.run_vmodel(vmc)
-    #vmr = pyv.get_result_from_coma(coma)
-    #pyv.column_density_plots(vmc, vmr, u.km, 1/u.cm**2, True)
-    #pyv.plot_fragment_sputter(vmr.fragment_sputter, dist_units=u.km,
-    #                sputter_units=1/u.cm**3, within_r=1000*u.km)
-
-    #OFF CENTERED
-    #pyv.column_density_plot_3d(vmc, vmr, x_min=-100000*u.km,
-    #                x_max=10000*u.km, y_min=-100000*u.km, y_max=10000*u.km,
-    #                grid_step_x=1000, grid_step_y=1000, r_units=u.km,
-    #                cd_units=1/u.cm**2)
-
-    #CENTERED
-    #pyv.column_density_plot_3d(vmc, vmr, x_min=-100000*u.km,
-    #                x_max=100000*u.km, y_min=-100000*u.km, y_max=100000*u.km,
-    #                grid_step_x=1000, grid_step_y=1000, r_units=u.km,
-    #                cd_units=1/u.cm**2)
-
-    #pyv.radial_density_plots(vmc, vmr, r_units=u.km, voldens_units=1/u.cm**3)
-    #return vmr
+    vmc = pyv._vm_config_from_yaml(fileName) #Creates the vmc object
+    coma = pyv.run_vmodel(vmc) #Creates the coma object
+    vmr = pyv.get_result_from_coma(coma) #Creates the vmr object
+    with io.StringIO() as buf, redirect_stdout(buf): #Gets all the print() from show_aperture_checks() and saves it to UIVariables
+        pyv.show_aperture_checks(coma)
+        UIVariables.ApertureChecks = buf.getvalue()
+    return vmc, vmr
 
 
 #Method def for running the program manually (ie: all input is done by user)
 def runManualProgram():
-    FileCreator.createDictionary()
-    FileCreator.newFile()
-    singleFileRun(UIVariables.FileName)
-    return
+    FileCreator.createDictionary() #Creates a new dict for the user inputs
+    FileCreator.newFile() #Creates a new yaml file
+    return singleFileRun(UIVariables.FileName) #Runs the program, returning a vmc and vmr
+    
 
 #Method def for running the program with file input (yaml)
 def runFileYamlProgram():
-    singleFileRun(UIVariables.FileName)
-    return
+    return singleFileRun(UIVariables.FileName)
+    
 
+#Method def for running the program with file input (pickle)
 def runFilePickleProgram():
-    pyv.read_results(UIVariables.FileName)
-    return
+    vmc = pyv.VectorialModelConfig(production=None, parent=None, fragment=pyv.Fragment(name ='unknown', v_photo=None, tau_T=None), comet=None, grid=None, etc=None) #Creates a default vmc
+    vmr = pyv.read_results(UIVariables.FileName) #Creates a vmr from the pickle
+    return vmc, vmr
 
+#Method def for testing if the program can read a given pickle file
 def pickleTest():
     try:
         pyv.read_results(UIVariables.FileName)
@@ -66,11 +52,11 @@ def pickleTest():
     except ModuleNotFoundError:
         return False
 
-#Method def for testing a 2 level deep dict value for it being either float/int.
+#Method def for testing a 2 level deep dict value for it being either float/int/bool any tpye.
 def dictTest2Var(dict, parent, child, type):
     try:
         testing = dict[f'{parent}'][f'{child}']
-        if (type == 'float'):
+        if (type == 'float'): #Test if the dict value is a float
             try:
                 float(testing)
                 if(float(testing) >= 0):
@@ -79,7 +65,7 @@ def dictTest2Var(dict, parent, child, type):
                     return False
             except ValueError:
                 return False
-        if (type == 'int'):
+        if (type == 'int'): #Test if the dict value is an int
             try:
                 int(testing)
                 if (int(testing) >= 0):
@@ -88,22 +74,22 @@ def dictTest2Var(dict, parent, child, type):
                     return False
             except ValueError:
                 return False
-        if (type == 'bool'):
+        if (type == 'bool'): #Test if the dict value is a bool
             try:
                 if((testing) == True or (testing == False)):
                     return True
             except ValueError:
                 return False
-        if (type == 'none'):
+        if (type == 'none'): #Test if the dict value exist as any type
             return True
     except KeyError:
         return False
 
 #Method def for testing all input files with the correct results
 def fileTest():
-    with open(f"{UIVariables.FileName}", 'r') as file:
+    with open(f"{UIVariables.FileName}", 'r') as file: #Opens the user yaml file
         dict = yaml.safe_load(file)
-        if(dictTest2Var(dict, 'comet', 'rh', 'float') == False):
+        if(dictTest2Var(dict, 'comet', 'rh', 'float') == False): #Test and sees if the dict value is a correct data type
             return False
         if(dictTest2Var(dict, 'comet', 'transform_applied', 'bool') == False):
             return False
@@ -119,30 +105,80 @@ def fileTest():
             return False
         if(dictTest2Var(dict, 'parent', 'sigma', 'float') == False):
             return False
-        if(dictTest2Var(dict, 'parent', 'tau_T', 'float') == False):
-            return False
         if(dictTest2Var(dict, 'parent', 'tau_d', 'float') == False):
            return False
         if(dictTest2Var(dict, 'parent', 'v_outflow', 'float') == False):
             return False
         if(dictTest2Var(dict, 'production', 'base_q', 'float') == False):
             return False
-        dict['etc'] = {}
-        dict['etc']['print_binned_times'] = UIVariables.PrintBinnedTimes
-        dict['etc']['print_column_density'] = UIVariables.PrintColumnDensity
-        dict['etc']['print_progress'] = UIVariables.PrintProgress
-        dict['etc']['print_radial_density'] = UIVariables.PrintRadialDensity
+        dict['etc'] = {} #Creates the etc section for the dict
+        dict['etc']['print_binned_times'] = True
+        dict['etc']['print_column_density'] = True
+        dict['etc']['print_progress'] = True
+        dict['etc']['print_radial_density'] = True
         dict['etc']['pyv_coma_pickle'] = UIVariables.PyvComaPickle
         dict['etc']['pyv_date_of_run'] = datetime.now()
-        dict['etc']['show_3d_column_density_centered'] = UIVariables.Show3dColumnDensityCentered
-        dict['etc']['show_3d_column_density_off_center'] = UIVariables.Show3dColumnDensityOffCenter
-        dict['etc']['show_agreement_check'] = UIVariables.ShowAgreementCheck
-        dict['etc']['show_aperture_checks'] = UIVariables.ShowApertureChecks
-        dict['etc']['show_column_density_plots'] = UIVariables.ShowColumnDensityPlots
-        dict['etc']['show_fragment_sputter'] = UIVariables.ShowFragmentSputter
-        dict['etc']['show_radial_plots'] = UIVariables.ShowRadialPlots
-        with open(f'{UIVariables.FileName}', 'w') as newerFile:
-            documents = yaml.dump(dict, newerFile)
+        dict['etc']['show_3d_column_density_centered'] = True
+        dict['etc']['show_3d_column_density_off_center'] = True
+        dict['etc']['show_agreement_check'] = True
+        dict['etc']['show_aperture_checks'] = True
+        dict['etc']['show_column_density_plots'] = True
+        dict['etc']['show_fragment_sputter'] = True
+        dict['etc']['show_radial_plots'] = True
+        with open(f'{UIVariables.FileName}', 'w') as newerFile: #Opens a new file
+            documents = yaml.dump(dict, newerFile) #Creates a new yaml file with the new etc section, replacing the old one
             newerFile.close()
         file.close()
     return True
+
+#Gets the radial plot from pyvectorial
+#Returns the figure
+def getRadialPlots(vmc, vmr):
+    return pyv.radial_density_plots(vmc, vmr, r_units=u.km, voldens_units=1/u.cm**3, show_plots=False)[1]
+
+#Gets the fragment sputter plot from pyvectorial
+#Returns the figure
+def getFragSputter(vmc, vmr):
+    return pyv.plot_fragment_sputter(vmr.fragment_sputter, dist_units=u.km, sputter_units=1/u.cm**3, within_r=1000*u.km, show_plots=False)[1]
+
+#Gets the column density from pyvectorial
+#Returns the figure
+def getColumnDensity(vmc, vmr):
+    return pyv.column_density_plots(vmc, vmr, u.km, 1/u.cm**2, show_plots=False)[1]
+
+#Gets the 3d column density plot from pyvectorial
+#Returns the figure
+def get3DColumnDensity(vmc, vmr):
+    return pyv.column_density_plot_3d(vmc, vmr, x_min=-100000*u.km,
+                   x_max=10000*u.km, y_min=-100000*u.km, y_max=10000*u.km,
+                    grid_step_x=1000, grid_step_y=1000, r_units=u.km,
+                    cd_units=1/u.cm**2, show_plots=False)[1]
+
+#Gets the 3d colum density plot centered from pyvectorial
+#Returns the figure
+def get3DColumnDensityCentered(vmc, vmr):
+    return pyv.column_density_plot_3d(vmc, vmr, x_min=-100000*u.km,
+                   x_max=100000*u.km, y_min=-100000*u.km, y_max=100000*u.km,
+                   grid_step_x=1000, grid_step_y=1000, r_units=u.km,
+                   cd_units=1/u.cm**2, show_plots=False)[1]
+
+#Gets the radial density from a given vmr
+def getPrintRadialDensity(vmr):
+    with io.StringIO() as buf, redirect_stdout(buf):
+        pyv.print_radial_density(vmr)
+        result = buf.getvalue()
+    return result
+    
+#Gets the column density from a given vmr
+def getPrintColumnDensity(vmr):
+    with io.StringIO() as buf, redirect_stdout(buf):
+        pyv.print_column_density(vmr)
+        result = buf.getvalue()
+    return result
+
+#Gets the agreement check from a given vmr
+def getAgreementCheck(vmr):
+    with io.StringIO() as buf, redirect_stdout(buf):
+        pyv.show_fragment_agreement(vmr)
+        result = buf.getvalue()
+    return result
