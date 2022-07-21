4a47ccc0871de63284de34cc3aa4ac9dc37b00a9
Jacob Duffy <jod0007@auburn.edu>
2022-07-21 14:41:58 -0500

    Added a folder

---
+++ Vectorial UI/UIVariables.py
@@ -0,0 +1,53 @@
+#Program that contains global variables used in the UI.
+#
+#Author: Jacob Duffy
+#Version: 7/16/2022
+
+#UI manager variables
+FileName = None
+DownFile = None
+PickleInputs = False
+
+#Production variable
+BaseQ = None
+
+#Time Variation variables
+TimeVariationType = None
+SinAmp = None
+SinPer = None
+SinDelta = None
+GausAmp = None
+GausSTD = None
+GausT_Max = None
+SquareAmp = None
+SquareDur = None
+SquareT_Start = None
+
+#Parent variables
+ParentName = None
+VOutflow = None
+TauD = None
+TauTParent = None
+Sigma = None
+TtoDRatio = None
+
+#Fragment variables
+FragmentName = None
+VPhoto = None
+TauTFragment = None
+
+#Comet variables
+CometName = None
+Rh = None
+CometDelta = None
+TransformMethod = None
+ApplyTransforMethod = False
+
+#Grid variables
+AngularPoints = None
+RadialPoints = None
+RadialSubsteps = None
+
+#Etc variables
+PyvComaPickle = None
+ApertureChecks = None
