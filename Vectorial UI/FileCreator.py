bb9cae018b16b0b0bd744c51453970751701ad6b
Jacob Duffy <jduffy0121@MacBook-Pro-14.local>
2022-08-23 12:03:31 -0500

    Added a data structure

--- Vectorial UI/FileCreator.py
+++ Vectorial UI/FileCreator.py
@@ -2,63 +2,61 @@
 #Uses a nested dict data type to write the file.
 #
 #Author: Jacob Duffy
-#Version: 7/16/2022
+#Version: 8/23/2022
 
-import UIVariables
-import yaml
 import os
+import yaml
 from datetime import datetime
 
 #Method to create a dictionary based on the variables in UIVariables.py
 #Returns the completed dictionary
-def createDictionary():
-
+def createDictionary(CurrentUIRun):
     #Creates the production dictionary
-    if(UIVariables.TimeVariationType == 'sine wave'):
-        params = {'amplitude' : UIVariables.SinAmp, 'period' : UIVariables.SinPer,
-        'delta' : UIVariables.SinDelta}
-        production = {'base_q' : UIVariables.BaseQ,
-        'time_variation_type' : UIVariables.TimeVariationType, 'params' : params}
-    elif(UIVariables.TimeVariationType == 'gaussian'):
-        params = {'amplitude' : UIVariables.GausAmp, 'std_dev' : UIVariables.GausSTD,
-        't_max' : UIVariables.GausT_Max}
-        production = {'base_q' : UIVariables.BaseQ,
-        'time_variation_type' : UIVariables.TimeVariationType, 'params' : params}
-    elif(UIVariables.TimeVariationType == 'square pulse'):
-        params = {'amplitude' : UIVariables.SquareAmp, 'duration' : UIVariables.SquareDur,
-        't_start' : UIVariables.SquareT_Start}
-        production = {'base_q' : UIVariables.BaseQ,
-        'time_variation_type' : UIVariables.TimeVariationType, 'params' : params}
+    if(CurrentUIRun.TimeVariationType == 'sine wave'):
+        params = {'amplitude' : CurrentUIRun.SinAmp, 'period' : CurrentUIRun.SinPer,
+        'delta' : CurrentUIRun.SinDelta}
+        production = {'base_q' : CurrentUIRun.BaseQ,
+        'time_variation_type' : CurrentUIRun.TimeVariationType, 'params' : params}
+    elif(CurrentUIRun.TimeVariationType == 'gaussian'):
+        params = {'amplitude' : CurrentUIRun.GausAmp, 'std_dev' : CurrentUIRun.GausSTD,
+        't_max' : CurrentUIRun.GausT_Max}
+        production = {'base_q' : CurrentUIRun.BaseQ,
+        'time_variation_type' : CurrentUIRun.TimeVariationType, 'params' : params}
+    elif(CurrentUIRun.TimeVariationType == 'square pulse'):
+        params = {'amplitude' : CurrentUIRun.SquareAmp, 'duration' : CurrentUIRun.SquareDur,
+        't_start' : CurrentUIRun.SquareT_Start}
+        production = {'base_q' : CurrentUIRun.BaseQ,
+        'time_variation_type' : CurrentUIRun.TimeVariationType, 'params' : params}
     else:
-        production = {'base_q' : UIVariables.BaseQ,
-        'time_variation_type' : UIVariables.TimeVariationType}   
+        production = {'base_q' : CurrentUIRun.BaseQ,
+        'time_variation_type' : CurrentUIRun.TimeVariationType}   
 
     #Creates the parent dictionary
-    parent = {'name' : UIVariables.ParentName, 
-    'v_outflow' : UIVariables.VOutflow, 
-    'tau_d' : UIVariables.TauD, 'tau_T' : UIVariables.TauTParent, 
-    'sigma' : UIVariables.Sigma, 'T_to_d_ratio' : UIVariables.TtoDRatio}
+    parent = {'name' : CurrentUIRun.ParentName, 
+    'v_outflow' : CurrentUIRun.VOutflow, 
+    'tau_d' : CurrentUIRun.TauD, 'sigma' : CurrentUIRun.Sigma, 
+    'T_to_d_ratio' : CurrentUIRun.TtoDRatio}
 
     #Creates the fragment dictionary
-    fragment = {'name' : UIVariables.FragmentName, 
-    'v_photo' : UIVariables.VPhoto, 
-    'tau_T' : UIVariables.TauTParent}
+    fragment = {'name' : CurrentUIRun.FragmentName, 
+    'v_photo' : CurrentUIRun.VPhoto, 
+    'tau_T' : CurrentUIRun.TauT}
 
     #Creates the comet dictionary
-    comet = {'name' : UIVariables.CometName, 'rh' : UIVariables.Rh, 
-    'delta' : UIVariables.CometDelta, 
-    'transform_method' : UIVariables.TransformMethod, 
-    'transform_applied' : UIVariables.ApplyTransforMethod}
+    comet = {'name' : CurrentUIRun.CometName, 'rh' : CurrentUIRun.Rh, 
+    'delta' : CurrentUIRun.CometDelta, 
+    'transform_method' : CurrentUIRun.TransformMethod, 
+    'transform_applied' : CurrentUIRun.ApplyTransforMethod}
 
     #Creates the grid dictionary
-    grid = {'radial_points' : UIVariables.RadialPoints, 
-    'angular_points' : UIVariables.AngularPoints, 
-    'radial_substeps' : UIVariables.RadialSubsteps}
+    grid = {'radial_points' : CurrentUIRun.RadialPoints, 
+    'angular_points' : CurrentUIRun.AngularPoints, 
+    'radial_substeps' : CurrentUIRun.RadialSubsteps}
 
     #Creates the etc dictionary
     etc = {'print_binned_times' : True, 'print_column_density' : True, 
     'print_progress' : True, 'print_radial_density' : True,
-    'pyv_coma_pickle' : UIVariables.PyvComaPickle,
+    'pyv_coma_pickle' : CurrentUIRun.PyvComaPickle,
     'pyv_date_of_run' : datetime.now(), 'show_3d_column_density_centered' : True,
     'show_3d_column_density_off_center' : True, 'show_agreement_check' : True,
     'show_aperture_checks' : True, 'show_column_density_plots' : True,
@@ -71,9 +69,9 @@ def createDictionary():
     return dict
 
 #Creates a new .yaml file called pyvectorial.yaml based on the return val of createDictionary()
-def newFileManual():
+def newFileManual(CurrentUIRun):
     with open(r'pyvectorial.yaml', 'w') as file:
-        documents = yaml.dump(createDictionary(), file)
+        documents = yaml.dump(createDictionary(CurrentUIRun), file)
 
 #Creates a new .yaml file saved to a filePath (including file name) with a given dict imput
 def newFileInputs(filePath, dict):
